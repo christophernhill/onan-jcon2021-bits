4466a197391008b5ee605631c9b0f77872e90d58
Chris Hill <cnh@mit.edu>
2021-07-07 12:22:42 -0400

    Update p6.sh

--- provision/aws/p6.sh
+++ provision/aws/p6.sh
@@ -4,6 +4,6 @@ wget https://gigenet.dl.sourceforge.net/project/turbovnc/2.2.6/turbovnc_2.2.6_am
 sudo apt-get install `pwd`/turbovnc_2.2.6_amd64.deb
 sudo apt-get install -y novnc websockify python-numpy
 
-# Boot commands
+# Boot commands (to work with ssh -L 6908:localhost:6908 -i key -l LOGIN and then http://localhost:6908 )
 /opt/TurboVNC/bin/vncserver
 websockify -D --web=/usr/share/novnc 6908 localhost:5901
