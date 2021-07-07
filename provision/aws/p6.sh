70d4bef131ba3db005ce4253fabf699c66abee7c
Chris Hill <cnh@mit.edu>
2021-07-06 23:22:05 -0400

    Create p6.sh

---
+++ provision/aws/p6.sh
@@ -0,0 +1,9 @@
+# Add VirtualGL and TurboVNC
+sudo apt-get install virtualgl
+wget https://gigenet.dl.sourceforge.net/project/turbovnc/2.2.6/turbovnc_2.2.6_amd64.deb
+sudo apt-get install `pwd`/turbovnc_2.2.6_amd64.deb
+sudo apt-get install -y novnc websockify python-numpy
+
+# Boot commands
+/opt/TurboVNC/bin/vncserver
+websockify -D --web=/usr/share/novnc 6908 localhost:5901
