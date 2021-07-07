3b3e1db78663c8d72632b760744da914d870139c
Chris Hill <cnh@mit.edu>
2021-07-07 15:57:49 -0400

    Update 05spack_and_openmpi.sh

--- provision/aws/05spack_and_openmpi.sh
+++ provision/aws/05spack_and_openmpi.sh
@@ -10,6 +10,9 @@ spack external find
 spack install openmpi +cuda
 bin/spack install openmpi '~atomics+cuda~cxx~cxx_exceptions+gpfs~internal-hwloc~java~legacylaunchers~lustre~memchecker~pmi~singularity~sqlite3+static+thread_multiple+vt+wrapper-rpath' fabrics=ucx schedulers=none
 
+apt install -y lmod
+source /etc/profile.d/lmod.sh
+spack module lmod refresh
 # Configureing JUlia for MPI
 # # To set up MPI may need
 # /home/ubuntu/projects/julia-1.6.1/bin/julia -e 'using Libdl; p=dlopen("libmpi", RTLD_LAZY; throw_error=false); p=dlopen("libmpi", RTLD_LAZY; throw_error=false); using Pkg; Pkg.build("MPI"; verbose=true)'
