9ce0d9bd7dc9a693f00be35850a185843dc47d67
Chris Hill <cnh@mit.edu>
2021-07-07 15:48:34 -0400

    Update p5.sh

--- provision/aws/p5.sh
+++ provision/aws/p5.sh
@@ -8,6 +8,7 @@ sudo apt-get install libssl-dev   # Needed by libevent
 spack compier list
 spack external find
 spack install openmpi +cuda
+bin/spack install openmpi '~atomics+cuda~cxx~cxx_exceptions+gpfs~internal-hwloc~java~legacylaunchers~lustre~memchecker~pmi~singularity~sqlite3+static+thread_multiple+vt+wrapper-rpath' fabrics=ucx schedulers=none
 
 # Configureing JUlia for MPI
 # # To set up MPI may need
