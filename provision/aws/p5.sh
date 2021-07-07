2cd1bf8fb5e83965d9cfd558bcfa493864c88917
Chris Hill <cnh@mit.edu>
2021-07-07 06:35:54 -0400

    Update p5.sh

--- provision/aws/p5.sh
+++ provision/aws/p5.sh
@@ -3,7 +3,8 @@ cd ~
 git clone https://github.com/spack/spack.git
 cd spack.git
 . share/spack/setup-env.sh
-sudo apt-get install gfortran
+sudo apt-get install gfortran.    # OpenMPI needs Fortran compiler too by default
+sudo apt-get install libssl-dev   # Needed by libevent
 spack compier list
 spack external find
 spack install openmpi +cuda
