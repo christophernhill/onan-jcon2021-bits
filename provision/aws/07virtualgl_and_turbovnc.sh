dfc8f600ad69d418cc72cb096afb516b494f972f
Chris Hill <cnh@mit.edu>
2021-07-07 21:34:50 -0400

    Update 07virtualgl_and_turbovnc.sh

--- provision/aws/07virtualgl_and_turbovnc.sh
+++ provision/aws/07virtualgl_and_turbovnc.sh
@@ -1,7 +1,7 @@
 # Add VirtualGL and TurboVNC
-sudo apt-get install virtualgl
+sudo apt-get install -y virtualgl
 wget https://gigenet.dl.sourceforge.net/project/turbovnc/2.2.6/turbovnc_2.2.6_amd64.deb
-sudo apt-get install `pwd`/turbovnc_2.2.6_amd64.deb
+sudo apt-get install -y `pwd`/turbovnc_2.2.6_amd64.deb
 sudo apt-get install -y novnc websockify python-numpy
 
 # Boot commands (to work with ssh -L 6908:localhost:6908 -i key -l LOGIN and then http://localhost:6908 )
