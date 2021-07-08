3122c9e0f7d1fe2ffcae89ee06b40f0463b0924e
Chris Hill <cnh@mit.edu>
2021-07-07 21:37:54 -0400

    Update 07virtualgl_and_turbovnc.sh

--- provision/aws/07virtualgl_and_turbovnc.sh
+++ provision/aws/07virtualgl_and_turbovnc.sh
@@ -5,5 +5,9 @@ sudo apt-get install -y `pwd`/turbovnc_2.2.6_amd64.deb
 sudo apt-get install -y novnc websockify python-numpy
 
 # Boot commands (to work with ssh -L 6908:localhost:6908 -i key -l LOGIN and then http://localhost:6908 )
+mkdir /home/$myuser/.vnc
+echo password | vncpasswd -f > /home/$myuser/.vnc/passwd
+chown -R $myuser:$myuser /home/$myuser/.vnc
+chmod 0600 /home/$myuser/.vnc/passwd
 /opt/TurboVNC/bin/vncserver
 websockify -D --web=/usr/share/novnc 6908 localhost:5901
